1ab848ae3ae89527e6b2123e4f56f9dd2719d251
zyangdev <zyang.dev@hotmail.com>
2021-06-29 11:48:39 +0800

    Update

---
+++ compile.bat
@@ -0,0 +1,1 @@
+g++ -c src/*.cpp -std=c++14 -O3 -Wall -m64 -I include -I D:/OpenGL/SDL2-w64/include && g++ *.o -o bin/release/main -s -L D:/OpenGL/SDL2-w64/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer && start bin/release/main
